170814c6c3c1feff2ebed81f8b84a392a4ecdc02
karimrir1 <50053541+karimrir1@users.noreply.github.com>
2022-02-10 20:05:31 +0300

    Actually get a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("org.karimrir1.WallMC")
+        .build();
+    /* const ver: &str = env!("CARGO_PKG_VERSION"); */
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .default_width(1284)
+            .default_height(695)
+            .title("WallMC v0.1.0")
+            .build();
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+        win.add(&button);
+        
+
+        // Don't forget to make all widgets visible.
+        win.show_all();
+    });
+
+    app.run();
 }
